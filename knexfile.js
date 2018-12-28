c4c031d5258bb85f512dcc3fc22cb76e6961cc24
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-27 17:34:32 -0800

    knexfile configured for deploy and postgres

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,46 @@
 // Update with your config settings.
+require('dotenv').config();
+
+const pg = require('pg');
+pg.defaults.ssl = true;
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './characterDatabase.sqlite3',
+    },
   },
 
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: 'knex_migrations',
+    },
+  },
 };
